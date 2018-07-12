a775f05c1f7bb9d290402c389acc45d4554792c3
Unknown <johnpalowitch@gmail.com>
2018-07-11 21:13:14 -0700

    Update test.

--- tests/testthat/testMtSigCommSingle.R
+++ tests/testthat/testMtSigCommSingle.R
@@ -1,4 +1,4 @@
-context("Overall SOCS Check")
+context("FOCS Backend Check")
 library(igraph)
 RUN_SEED <- 123454321
 
@@ -7,11 +7,6 @@ test_that("A random community is scored appropriately", {
   network <- degree.sequence.game(rep(5, 20), method="simple")
   membership <- cluster_louvain(network)$membership
   comm <- which(membership == 1)
-  adj <- get.adjacency(network)
-  d <- degree(network)
-  N <- length(V(network))
-  edges <- get.edgelist(network)
-  m <- nrow(edges)
-  pval <- .SOCS(comm, adj, edges, d, N, m)
-  expect_equal(signif(pval, 7), 0.3274649)
+  score <- unlist(FOCS(list(comm), network, p=0.25))
+  expect_equal(signif(score, 7), 0.2162592)
 })
