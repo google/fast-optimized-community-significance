179992c42251a493a4fe2048ace3de6145478579
jpalowitch <johnpalowitch@gmail.com>
2018-10-09 21:38:32 +0000

    Add test of node removals for unipartite.

--- tests/testthat/testFocsBackend.R
+++ tests/testthat/testFocsBackend.R
@@ -165,3 +165,121 @@ test_that("p-score function works on small imbalanced barbell graph.", {
   expect_equal(signif(p.score.obj$ps.lower[2], 7), 0)
   expect_equal(p.score.obj$worst, 6)
 })
+
+test_that("FOCS correctly scores 5-clique in imbalanced barbell graph.", {
+
+  # Feed FOCS the imbalanced barbell graph, the community {1, 2, 3, 4, 5}, and
+  # p parameter 0.5, so that it will test the first two worst nodes. 
+  # Also, we set nrand=1 so we can test the randomized scoring.
+
+  # At first we have C' with 3 * 2 + 1 = 7 stubs, and C with 1 outgoing stub.
+  # Removing node 5 from C adds 4 outgoing stubs to C but removes 1.
+  # Removing any other nodes in C adds 4 outgoing stubs to C.
+  # So the p-score for 5 should be:
+  #   phyper(4 - 1, 4, 7, 5, lower.tail=FALSE) = 0.01515152.
+  # and for {1...4}:
+  #   phyper(4 - 1, 5, 7, 4, lower.tail=FALSE) = 0.01010101.
+  # ...with lower bounds of zero for both, due to reasoning in previous test.
+  # So the first set of worst nodes are 5 and 1, and 5 will be removed.
+
+  # If the seed is set at 543212345, the .fScore function should perform a
+  # calculation identical to these commands:
+  #   p <- 0.5
+  #   nrand <- 1
+  #   k <- round(5 * p)
+  #   fMat <- matrix(0, nrow=nrand, ncol=k)
+  #   RUN_SEED <- 543212345
+  #   set.seed(RUN_SEED)
+  #
+  #   # First round of testing.
+  #   p.rand <- runif(2, c(0.0, 0.0), c(0.01515152, 0.01010101))
+  #   N <- 8
+  #   NC <- 5
+  #   if (p.rand[1] > p.rand[2]) {
+  #     test.score <- 1 - ((1 - p.rand[1]) / (1 - p.rand[2]))^(N - NC + 1)  
+  #   } else {
+  #     test.score <- 1 - ((1 - p.rand[2]) / (1 - p.rand[1]))^(N - NC + 1)
+  #   }
+  #   fMat[1, 1] <- test.score
+  #
+  # After 5 is removed, C = {1, 2, 3, 4}, C' nodes have the following stub counts:
+  #   5: 5, 6: 3, 7: 2, 7: 2,
+  # so 12 stubs total. C has 4 outgoing stubs to node 5 in C'. Removing any node in C
+  # adds 3 outgoing stubs but removes 1 (the one it had to node 5). So p-scores are:
+  #   phyper(3 - 1, 6, 12, 4, lower.tail=FALSE) = 0.08333333. 
+  # with lower bounds:
+  #   phyper(3, 6, 12, 4, lower.tail=FALSE) = 0.004901961.
+  #
+  # With the worst node removed, .focs should call .fScore again (i = 2) with the above
+  # upper/lower bounds:
+  #
+  #   # Second round of testing.
+  #   p.rand <- runif(2, c(0.0, 0.0), c(0.08333333, 0.004901961))
+  #   N <- 8
+  #   NC <- 4
+  #   if (p.rand[1] > p.rand[2]) {
+  #     test.score <- 1 - ((1 - p.rand[1]) / (1 - p.rand[2]))^(N - NC + 1)
+  #   } else {
+  #     test.score <- 1 - ((1 - p.rand[2]) / (1 - p.rand[1]))^(N - NC + 1)
+  #   }
+  #   fMat[1, 2] <- test.score
+  #
+  toy.graph <- AddClique(3, AddClique(5))
+  toy.graph <- rbind(toy.graph, matrix(c(5, 6), ncol=2))
+  graph.stats <- ComputeGraphStatistics(toy.graph)
+
+  p.score.obj <- .GetPscoreObject(comm=1:5, graph.stats$adj, toy.graph,
+                                  graph.stats$d, graph.stats$N, graph.stats$m)
+  expect_equal(signif(p.score.obj$ps.upper[1], 7), 0.01515152)
+  expect_equal(signif(p.score.obj$ps.lower[1], 7), 0)
+  expect_equal(signif(p.score.obj$ps.upper[2], 7), 0.01010101)
+  expect_equal(signif(p.score.obj$ps.lower[2], 7), 0)
+  expect_equal(p.score.obj$worst, 5)
+  
+  p.score.obj <- .GetPscoreObject(comm=1:4, graph.stats$adj, toy.graph,
+                                  graph.stats$d, graph.stats$N, graph.stats$m)
+  expect_equal(signif(p.score.obj$ps.upper[1], 7), 0.08333333)
+  expect_equal(signif(p.score.obj$ps.lower[1], 7), 0.004901961)
+  expect_equal(signif(p.score.obj$ps.upper[2], 7), 0.08333333)
+  expect_equal(signif(p.score.obj$ps.lower[2], 7), 0.004901961)
+  expect_equal(p.score.obj$worst, 1) 
+
+  p <- 0.5
+  nrand <- 1
+  k <- round(5 * p)
+  fMat <- matrix(0, nrow=nrand, ncol=k)
+  RUN_SEED <- 543212345
+  set.seed(RUN_SEED)
+  # First round of testing: note this does not merely duplicate code,
+  # we are (1) hard-coding the computed p-scores, and 
+  # (2) manually executing the two loop rounds.
+  p.rand <- runif(2, c(0.0, 0.0), c(0.01515152, 0.01010101))
+  N <- 8
+  NC <- 5
+  if (p.rand[1] > p.rand[2]) {
+    test.score <- 1 - ((1 - p.rand[1]) / (1 - p.rand[2]))^(N - NC + 1)
+  } else {
+    test.score <- 1 - ((1 - p.rand[2]) / (1 - p.rand[1]))^(N - NC + 1)
+  }
+  fMat[1, 1] <- test.score
+
+  # Second round of testing: note this does not merely duplicate code,
+  # we are (1) hard-coding the computed p-scores, and 
+  # (2) manually executing the two loop rounds.
+  p.rand <- runif(2, c(0.0, 0.0), c(0.08333333, 0.004901961))
+  N <- 8
+  NC <- 4
+  if (p.rand[1] > p.rand[2]) {
+    test.score <- 1 - ((1 - p.rand[1]) / (1 - p.rand[2]))^(N - NC + 1)
+  } else {
+    test.score <- 1 - ((1 - p.rand[2]) / (1 - p.rand[1]))^(N - NC + 1)
+  }
+  fMat[1, 2] <- test.score
+  
+  expect_equal(signif(median(as.vector(apply(fMat, 1, min))), 7),
+               0.004881726)
+  set.seed(RUN_SEED)
+  expect_equal(.focs(1:5, graph.stats$adj, toy.graph, graph.stats$d, 
+                     graph.stats$N, graph.stats$m, p=0.25, nrand=1),
+               signif(median(as.vector(apply(fMat, 1, min))), 7))
+})
--- tests/testthat/testFocsBackendBipartite.R
+++ tests/testthat/testFocsBackendBipartite.R
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
  
-context("FOCS Backend Check")
+context("FOCS Backend Check - Bipartite")
 library(igraph)
 RUN_SEED <- 123454321
 
